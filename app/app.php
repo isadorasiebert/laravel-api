3c05e21b66c856c5c17c0b8a15d3f7930f832368
isadoracsiebert <isadoracsiebert@usp.br>
2022-01-16 19:06:03 -0300

    api-app

---
+++ app/app.php
@@ -0,0 +1,21 @@
+<?php
+
+define('API_BASE', 'http://localhost:8080/laravel-api/api/?option=');
+
+// echo API_BASE . 'status';
+
+echo '<p>Aplicação</p>';
+
+$resultado = api_request('status');
+
+echo '<pre>';
+print_r($resultado);
+
+function api_request($option) {
+    $client = curl_init(API_BASE . $option);
+    curl_setopt($client, CURLOPT_RETURNTRANSFER, true);
+    $response = curl_exec($client);
+
+    // true = array associativo
+    return json_decode($response, true);
+}
